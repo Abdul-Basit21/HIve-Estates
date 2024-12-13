a14f1310d8c6ee046d88b84073080d7de5c30c9d
Abdul-Basit21 <abdul.basit@mage4.com>
2024-12-13 17:49:52 +0500

    property listing

--- wp-content/plugins/HiveEstates-addon/widgets/property-list-sale.php
+++ wp-content/plugins/HiveEstates-addon/widgets/property-list-sale.php
@@ -98,22 +98,52 @@ class Property_List_Sale extends \Elementor\Widget_Base
                 foreach ($select_properties as $properties) {
                     $property_img = get_the_post_thumbnail($properties);
                     $property_cat = get_the_category($properties);
+                    $property_tag = get_the_tags($properties);
                 ?>
-                    <div class="col-3">
-                        <?php echo $property_img; ?>
-                        <h2><?php echo get_the_title($properties); ?></h2>
+                    <div class="col-4 porperty-list">
                         <?php
-                        foreach ($property_cat as $cat) {
+                        foreach ($property_tag as $tag) {
                         ?>
-                            <p><?php echo $cat->name; ?></p>
+                            <!-- <span class="prop-icon tag">
+                                <i class="ri-home-office-line"></i>
+                                <?php echo $tag->name; ?>
+                            </span> -->
                         <?php
                         };
                         ?>
-                        <p><?= get_field('property_price', $properties); ?></p>
-                        <p><?= get_field('beds', $properties); ?></p>
-                        <p><?= get_field('baths', $properties); ?></p>
-                        <p><?= get_field('rooms', $properties); ?></p>
-                        <p><?= get_field('address', $properties); ?></p>
+                        <div class="property-img-wrapper">
+                            <a href="<?php echo the_permalink($properties); ?>">
+                                <?php echo $property_img; ?>
+                            </a>
+                        </div>
+                        <span class="property-price">$<?= get_field('property_price', $properties); ?></span>
+                        <a href="<?php echo the_permalink($properties); ?>" class="property-title"><?php echo get_the_title($properties); ?></a>
+                        <p class="property-address">
+                            <i class="fa-solid fa-location-arrow"></i>
+                            <?= get_field('address', $properties); ?>
+                        </p>
+                        <span class="prop-icon beds">
+                            <i class="ri-hotel-bed-line"></i>
+                            Beds : <?= get_field('beds', $properties); ?>
+                        </span>
+                        <span class="prop-icon baths">
+                            <i class="fa-solid fa-bath"></i>
+                            Baths : <?= get_field('baths', $properties); ?>
+                        </span>
+                        <span class="prop-icon area">
+                            <i class="ri-codepen-line"></i>
+                            Area : <?= get_field('area', $properties); ?>
+                        </span>
+                        <?php
+                        foreach ($property_cat as $cat) {
+                        ?>
+                            <span class="prop-icon category prop-footer">
+                                <i class="ri-stack-line"></i>
+                                <strong>Category :</strong><?php echo $cat->name; ?>
+                            </span>
+                        <?php
+                        };
+                        ?>
                     </div>
                 <?php
                 }

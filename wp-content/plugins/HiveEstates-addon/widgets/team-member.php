69190d7b2d02a25f08fe8ed13a77883559440fb1
Abdul-Basit21 <abdul.basit@mage4.com>
2024-12-19 17:34:34 +0500

    social-team link

--- wp-content/plugins/HiveEstates-addon/widgets/team-member.php
+++ wp-content/plugins/HiveEstates-addon/widgets/team-member.php
@@ -73,6 +73,62 @@ class Our_Team_Widget extends \Elementor\Widget_Base
             ]
         );
 
+        
+        $repeater->add_control(
+			'facebook_link',
+			[
+				'label' => esc_html__( 'Facebook Link', 'hiveestates-addon' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'options' => [ 'url', 'is_external', 'nofollow' ],
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					// 'custom_attributes' => '',
+				],
+				'label_block' => true,
+			]
+		);
+        $repeater->add_control(
+			'instagram_link',
+			[
+				'label' => esc_html__( 'Instagram Link', 'hiveestates-addon' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'options' => [ 'url', 'is_external', 'nofollow' ],
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					// 'custom_attributes' => '',
+				],
+				'label_block' => true,
+			]
+		);
+        $repeater   ->add_control(
+			'twitter_link',
+			[
+				'label' => esc_html__( 'Twitter Link', 'hiveestates-addon' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'options' => [ 'url', 'is_external', 'nofollow' ],
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					// 'custom_attributes' => '',
+				],
+				'label_block' => true,
+			]
+		);
+        $repeater->add_control(
+            'phone_number',
+            [
+                'label' => esc_html__('Number', 'hiveestates-addon'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => esc_html__('Number', 'hiveestates-addon'),
+                'label_block' => true,
+            ]
+        );
+
         $this->add_control(
             'team_members',
             [
@@ -84,11 +140,19 @@ class Our_Team_Widget extends \Elementor\Widget_Base
                         'image' => esc_html__('Image', 'hiveestates-addon'),
                         'name' => esc_html__('Name', 'hiveestates-addon'),
                         'designation' => esc_html__('Designation', 'hiveestates-addon'),
+                        'facebook_link' => esc_html__('Facebook Link', 'hiveestates-addon'),
+                        'instagram_link' => esc_html__('Instagram Link', 'hiveestates-addon'),
+                        'twitter_link' => esc_html__('Twitter Link', 'hiveestates-addon'),
+                        'phone_number' => esc_html__('Number', 'hiveestates-addon'),
                     ],
                     [
                         'image' => esc_html__('Image', 'hiveestates-addon'),
                         'name' => esc_html__('Name', 'hiveestates-addon'),
                         'designation' => esc_html__('Designation', 'hiveestates-addon'),
+                        'facebook_link' => esc_html__('Facebook Link', 'hiveestates-addon'),
+                        'instagram_link' => esc_html__('Instagram Link', 'hiveestates-addon'),
+                        'twitter_link' => esc_html__('Twitter Link', 'hiveestates-addon'),
+                        'phone_number' => esc_html__('Number', 'hiveestates-addon'),
                     ],
                 ],
                 'title_field' => '{{{ name }}}',
@@ -132,10 +196,10 @@ class Our_Team_Widget extends \Elementor\Widget_Base
                             <h2 class="member-name"><?php echo $member['name']; ?></h2>
                             <div class="member-designation"><?php echo $member['designation']; ?></div>
                             <div class="member-social-wrapper">
-                                <a href=""><i class="fa-brands fa-facebook-f"></i></a>
-                                <a href=""><i class="fa-brands fa-instagram"></i></a>
-                                <a href=""><i class="fa-brands fa-twitter"></i></a>
-                                <a href=""><i class="fa-solid fa-phone"></i></a>
+                                <a href="<?php echo  $member['facebook_link']['url']; ?>"><i class="fa-brands fa-facebook-f"></i></a>
+                                <a href="<?php echo  $member['instagram_link']['url']; ?>"><i class="fa-brands fa-instagram"></i></a>
+                                <a href="<?php echo  $member['twitter_link']['url']; ?>"><i class="fa-brands fa-twitter"></i></a>
+                                <a href="tel:<?php echo  $member['phone_number']; ?>"><i class="fa-solid fa-phone"></i></a>
                             </div>
                         </div>
                     </div>

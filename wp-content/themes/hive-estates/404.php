3610912202257928c67c1fafd55657bd82dbab42
Abdul-Basit21 <abdul.basit@mage4.com>
2024-12-20 21:21:32 +0500

    404 error

--- wp-content/themes/hive-estates/404.php
+++ wp-content/themes/hive-estates/404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -10,51 +11,40 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+	<div class="container-fluid inner-banner p-0">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 inner-banner-wrapper">
+					<h1 class="inner-title">Page Not Find</h1>
+					<?php if (function_exists('yoast_breadcrumb')) {
+						yoast_breadcrumb('<nav aria-label="You are here:" role="navigation"><ul class="breadcrumbs">', '</ul></nav>');
+					} ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section class="error-404 not-found container d-flex justify-content-center align-items-center">
+		<div class="row">
+			<div class="col-12">
+				<header class="page-header">
+					<img src="<?= get_field('404_image', 'option'); ?>" alt="">
+					<h1 class="page-title"><?php esc_html_e('Oops! page not found.', 'hive-estates'); ?></h1>
+				</header><!-- .page-header -->
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'hive-estates' ); ?></h1>
-			</header><!-- .page-header -->
+				<div class="page-content">
+					<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'hive-estates'); ?></p>
+				</div><!-- .page-content -->
+				<div class="hive-btn">
+					<a href="/">
+						<span>Back To Homepage</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section><!-- .error-404 -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'hive-estates' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'hive-estates' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$hive_estates_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'hive-estates' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$hive_estates_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_footer();

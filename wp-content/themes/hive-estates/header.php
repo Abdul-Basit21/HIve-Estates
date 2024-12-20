a4c313fc3b339f12c7537212819fae2ce2c03123
Abdul-Basit21 <abdul.basit@mage4.com>
2024-12-20 20:27:08 +0500

    fonts

--- wp-content/themes/hive-estates/header.php
+++ wp-content/themes/hive-estates/header.php
@@ -19,6 +19,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+
 </head>
 
 <body <?php body_class(); ?>>

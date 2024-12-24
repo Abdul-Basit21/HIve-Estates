3c280a361e66b9d6ecddd49bda697eacdff37114
Abdul-Basit21 <abdul.basit@mage4.com>
2024-12-24 22:38:02 +0500

    blog detail

--- wp-content/themes/hive-estates/single.php
+++ wp-content/themes/hive-estates/single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -10,30 +11,43 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+	<div class="container-fluid inner-banner p-0">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 inner-banner-wrapper">
+					<h1 class="inner-title"><?php the_title() ?></h1>
+					<?php if (function_exists('yoast_breadcrumb')) {
+						yoast_breadcrumb('<nav aria-label="You are here:" role="navigation"><ul class="breadcrumbs">', '</ul></nav>');
+					} ?>
+				</div>
+			</div>
+		</div>
+	</div>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+	<?php
+	while (have_posts()) :
+		the_post();
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'hive-estates' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'hive-estates' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+		get_template_part('template-parts/content', get_post_type());
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		the_post_navigation(
+			array(
+				'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'hive-estates') . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'hive-estates') . '</span> <span class="nav-title">%title</span>',
+			)
+		);
 
-		endwhile; // End of the loop.
-		?>
+		// If comments are open or we have at least one comment, load up the comment template.
+		if (comments_open() || get_comments_number()) :
+			comments_template();
+		endif;
 
-	</main><!-- #main -->
+	endwhile; // End of the loop.
+	?>
+
+</main><!-- #main -->
 
 <?php
 get_sidebar();

78bb649d559fcc5aa07f21b60e13142870d36e4b
Abdul-Basit21 <abdul.basit@mage4.com>
2024-12-24 20:03:58 +0500

    property detail gallery

--- wp-content/themes/hive-estates/single-properties.php
+++ wp-content/themes/hive-estates/single-properties.php
@@ -15,16 +15,16 @@
 </div>
 <div class="container property-detail-container">
     <div class="row">
-        <div class="col-8">
+        <div class="col-9">
             <div class="property-info-top">
                 <div class="info-top-left">
                     <span class="prop-tag">
                         <?php
                         $property_tags = get_the_tags($property_id);
                         if ($property_tags): ?>
-                                <?php foreach ($property_tags as $tag): ?>
-                                    <?php echo esc_html($tag->name); ?>
-                                <?php endforeach; ?>
+                            <?php foreach ($property_tags as $tag): ?>
+                                <?php echo esc_html($tag->name); ?>
+                            <?php endforeach; ?>
                         <?php endif;
                         ?>
                     </span>
@@ -34,9 +34,39 @@
                         <span class="prop-detail-location"><?= get_field('address', $property_id); ?></span>
                     </p>
                 </div>
+                <div class="info-top-right">
+                    <span class="prop-detail-price">$<?= get_field('property_price', $property_id); ?></span>
+                    <span class="prop-detail-area"><?= get_field('area', $property_id); ?></span>
+                </div>
+            </div>
+            <div class="property-detail-gallery">
+                <div class="outer">
+                    <div id="big" class="owl-carousel owl-theme">
+                        <?php
+                        $property_gallery = get_field('property_gallery', $property_id);
+                        foreach ($property_gallery as $property_gallery) {
+                        ?>
+                            <a href="<?php echo $property_gallery; ?>" data-fancybox="gallery">
+                                <img src="<?php echo $property_gallery; ?>" alt="">
+                            </a>
+                        <?php
+                        };
+                        ?>
+                    </div>
+                    <div id="thumbs" class="owl-carousel owl-theme">
+                        <?php
+                        $property_gallery = get_field('property_gallery', $property_id);
+                        foreach ($property_gallery as $property_gallery) {
+                        ?>
+                            <img src="<?php echo $property_gallery; ?>" alt="">
+                        <?php
+                        };
+                        ?>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-4"></div>
+        <div class="col-3"></div>
     </div>
 </div>
 
